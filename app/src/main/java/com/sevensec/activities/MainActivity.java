02be6e516299e094e53da79781b1d4c1b72d8f51
shweta-7Span <shweta@7span.com>
2023-02-17 18:36:38 +0530

    Show Permission's Page if it is not allowed & ask permissions by show alert dialog

--- app/src/main/java/com/sevensec/activities/MainActivity.java
+++ app/src/main/java/com/sevensec/activities/MainActivity.java
@@ -5,6 +5,8 @@ import static com.sevensec.utils.Constants.STR_FAV_APP_LIST;
 import static com.sevensec.utils.Utils.isAccessGranted;
 import static com.sevensec.utils.Utils.isDrawOverlayPermissionGranted;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -14,21 +16,23 @@ import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.sevensec.repo.FireStoreDataOperation;
 import com.sevensec.R;
 import com.sevensec.adapter.MyListAdapter;
 import com.sevensec.databinding.ActivityMainBinding;
-import com.sevensec.service.SaveMyAppsService;
 import com.sevensec.model.AppInfoModel;
+import com.sevensec.repo.FireStoreDataOperation;
+import com.sevensec.service.SaveMyAppsService;
 import com.sevensec.utils.Constants;
 import com.sevensec.utils.SharedPref;
 
@@ -39,11 +43,9 @@ import java.util.List;
 
 public class MainActivity extends FireStoreDataOperation {
 
-    String TAG;
-    String DEVICE_ID;
-    PowerManager pm;
-    List<String> favAppList;
+    String TAG = getClass().getName();
     ActivityMainBinding binding;
+    PowerManager pm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +53,44 @@ public class MainActivity extends FireStoreDataOperation {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         pm = (PowerManager) getSystemService(POWER_SERVICE);
-        TAG = getApplicationContext().getClass().getName();
-        DEVICE_ID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-        Log.d(TAG, "onCreate DEVICE_ID: " + DEVICE_ID);
-
         SharedPref.init(getApplicationContext());
-        SharedPref.writeString(STR_DEVICE_ID, DEVICE_ID);
 
-        favAppList = SharedPref.readListString(STR_FAV_APP_LIST);
+        checkPermission();
 
+        binding.btnPermission.setOnClickListener(view -> askPermissions());
+    }
+
+    private void checkPermission() {
+        if (isAccessGranted(getApplicationContext()) &&
+                isDrawOverlayPermissionGranted(getApplicationContext()) &&
+                pm.isIgnoringBatteryOptimizations(Constants.APP_PACKAGE_NAME)) {
+
+            Log.w(TAG, "onActivityResult All Permissions Granted: ");
+
+            //Get Installed App list & show the list after sort it
+            loadInstalledApps();
+            binding.llPermission.setVisibility(View.GONE);
+            binding.recyclerView.setVisibility(View.VISIBLE);
+
+            //Start service
+            startService(new Intent(this, SaveMyAppsService.class));
+
+            //Store DEVICE_ID in Preference
+            String DEVICE_ID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+            Log.d(TAG, "onCreate DEVICE_ID: " + DEVICE_ID);
+            SharedPref.writeString(STR_DEVICE_ID, DEVICE_ID);
+
+            //Store DEVICE_ID in FireStore
+            checkDeviceIsStored(DEVICE_ID);
+
+        }else{
+            Log.w(TAG, "onActivityResult All Permissions NOT Granted: ");
+            binding.llPermission.setVisibility(View.VISIBLE);
+            binding.recyclerView.setVisibility(View.GONE);
+        }
+    }
+
+    private void loadInstalledApps() {
         PackageManager packageManager = getApplicationContext().getPackageManager();
         /*Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         //mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -114,74 +145,94 @@ public class MainActivity extends FireStoreDataOperation {
             }
         });
 
+        //Get Selected App list and sort the app list to show the selected apps on top
+        List<String> favAppList = SharedPref.readListString(STR_FAV_APP_LIST);
+
         if (favAppList.size() > 0) {
             //Show Selected Apps on Top
             Collections.sort(appInfoModelList, new Comparator<AppInfoModel>() {
                 @Override
                 public int compare(AppInfoModel appInfoModel, AppInfoModel t1) {
 
-                    if (favAppList.contains(t1.getPackageName()))
-                        return 1;
-                    else if (favAppList.contains(appInfoModel.getPackageName()))
-                        return -1;
-                    else
-                        return 0;
+                    if (favAppList.contains(t1.getPackageName())) return 1;
+                    else if (favAppList.contains(appInfoModel.getPackageName())) return -1;
+                    else return 0;
                 }
             });
         }
 
         Log.i(TAG, "onCreate appInfoModelList length after sorting: " + appInfoModelList.size());
 
-
-
         MyListAdapter adapter = new MyListAdapter(appInfoModelList, favAppList);
-        binding.recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(),
-                DividerItemDecoration.VERTICAL));
+        binding.recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
         binding.recyclerView.setHasFixedSize(true);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView.setAdapter(adapter);
-
-        askPermissions();
     }
 
-    ActivityResultLauncher<Intent> startActivityIntent = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    // Add same code that you want to add in onActivityResult method
-                    Log.d(TAG, "onActivityResult: ");
-                    askPermissions();
-                }
-            });
+    ActivityResultLauncher<Intent> startActivityIntent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            // Add same code that you want to add in onActivityResult method
+            Log.d(TAG, "onActivityResult: ");
+            checkPermission();
+            askPermissions();
+        }
+    });
 
     private void askPermissions() {
         if (!isAccessGranted(getApplicationContext())) {
-            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-            startActivityIntent.launch(intent);
+            showPermissionDialog("Usage Access Permission",
+                    "Find the 7Sec app in the list and allow the Usage Access Permission.\n\nThen, come back.",
+                    101);
         } else {
             Log.e(TAG, "askPermissions app: " + Constants.APP_PACKAGE_NAME);
             Log.e(TAG, "askPermissions isBatteryOptimized: " + pm.isIgnoringBatteryOptimizations(Constants.APP_PACKAGE_NAME));
 
-            if (!pm.isIgnoringBatteryOptimizations(Constants.APP_PACKAGE_NAME)) {
-                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + Constants.APP_PACKAGE_NAME));
-                startActivityIntent.launch(intent);
-
+            if (!isDrawOverlayPermissionGranted(getApplicationContext())) {
+                showPermissionDialog("Overlay Permission",
+                        "Find the 7Sec app in the list and allow the Overlay Permission.\n\nThen, come back.",
+                        102);
             } else {
-                if (!isDrawOverlayPermissionGranted(getApplicationContext())) {
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                            Uri.parse("package:" + Constants.APP_PACKAGE_NAME));
-                    startActivityIntent.launch(intent); //It will call onActivityResult Function After you press Yes/No and go Back after giving permission
-                } else {
-                    Log.w(TAG, "onActivityResult All Permissions Granted: ");
-
-                    //start service
-                    startService(new Intent(this, SaveMyAppsService.class));
-
-                    checkDeviceIsStored(DEVICE_ID);
+                if (!pm.isIgnoringBatteryOptimizations(Constants.APP_PACKAGE_NAME)) {
+                    showPermissionDialog("Battery Optimization Permission",
+                            "Take out the Battery Optimization for 7Sec to run in the background.",
+                            103);
                 }
             }
+
         }
+    }
+
+    private void showPermissionDialog(String title, String description, int permissionCode) {
+        new AlertDialog
+                .Builder(MainActivity.this, R.style.MyAlertDialogTheme)
+                .setTitle(title)
+                .setMessage(description)
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (permissionCode == 101) {
+                            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+                            startActivityIntent.launch(intent);
+
+                        } else if (permissionCode == 102) {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + Constants.APP_PACKAGE_NAME));
+                            startActivityIntent.launch(intent); //It will call onActivityResult Function After you press Yes/No and go Back after giving permission
+
+                        } else if (permissionCode == 103) {
+                            Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                            intent.setData(Uri.parse("package:" + Constants.APP_PACKAGE_NAME));
+                            startActivityIntent.launch(intent);
+                        }
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton("Cancel", null)
+//                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
