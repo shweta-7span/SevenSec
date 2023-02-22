4409466142228f773c7ebad489839ab962437d41
shweta-7Span <shweta@7span.com>
2023-02-22 15:26:07 +0530

    Finish the 'Splash' when open 'MainActivity'. So, when user do back from 'MainActivity' then app will close instead of go to 'Splash' screen.

--- app/src/main/java/com/sevensec/activities/SplashActivity.java
+++ app/src/main/java/com/sevensec/activities/SplashActivity.java
@@ -26,11 +26,9 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
         }, 3000);
     }
 }
